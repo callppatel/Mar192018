7eba33d3ec95e641dda0f84c08a3211cccb73803
callppatel <callppatel.gmail.com>
2018-03-19 12:43:26 -0400

    hi

---
+++ my-app/src/test/java/com/acn/app/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package com.acn.app;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	@Test
+	public void HelloWorldPrint()
+	{
+			System.out.println("Hello World Printout" );
+	}
+}
